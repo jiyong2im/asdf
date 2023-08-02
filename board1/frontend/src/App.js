09d02461b02eb96b04a56538941c84f9832dfde0
정지용 <jjy4879@naver.com>
2023-08-02 19:01:06 +0900

    home

---
+++ board1/frontend/src/App.js
@@ -0,0 +1,18 @@
+/* App.js */
+import {Route, Routes} from "react-router-dom";
+import BoardList from "./routes/BoardList";
+import Home from "./routes/Home";
+import React from "react";
+import BoardDetail from "./routes/BoardDetail";
+
+function App() {
+    return (
+        <Routes>
+            <Route path="/" element={<Home/>}/>
+            <Route path="/board" element={<BoardList/>}/>
+            <Route path="/board/:idx" element={<BoardDetail/>}/>
+        </Routes>
+    );
+}
+
+export default App;
